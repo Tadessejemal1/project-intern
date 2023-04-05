f467b9b53b4fa7db84dd324a16ea3cb8c272e65f
Tadesse Jemal <tadessejemal63@gmail.com>
2023-04-05 20:09:34 +0300

    add home components

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <img src = {require('../../assets/room-1' + '.png')} alt='icon is not available' className='w-6 h-6' />
+      </div>
+    </div>
+  )
+}
+
+export default Home
